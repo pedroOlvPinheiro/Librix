7454049711ea0fd10e97e5aa252ee611795fc9bb
Pedro Oliveira <pedroolvcontato@gmail.com>
2026-02-04 15:19:08 -0300

    feat(user): adiciona enum role e coluna role  para RBAC

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -2,11 +2,15 @@ import { Column, Entity, OneToMany, OneToOne } from 'typeorm';
 import { Content } from './content.entity';
 import { Loan } from './loan.entity';
 import { Auth } from './auth.entity';
+import { RoleEnum } from 'src/utils/enum/role.enum';
 
 @Entity('user')
 export class User extends Content {
   @Column({ type: 'varchar', nullable: false })
   name: string;
+
+  @Column({ type: 'enum', nullable: false, default: RoleEnum.USER })
+  role: RoleEnum;
 
   @OneToMany(() => Loan, (loan) => loan.user)
   loans: Loan[];
