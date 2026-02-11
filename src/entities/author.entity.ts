a4bb21afe3a52968badda144aba86574e34a8130
Pedro Oliveira <pedroolvcontato@gmail.com>
2026-02-11 13:40:09 -0300

    feat(catalog): adiciona entidade Author e estabelece relação N:N com Book

---
+++ src/entities/author.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, ManyToMany } from 'typeorm';
+import { Content } from './content.entity';
+import { Book } from './book.entity';
+
+@Entity('author')
+export class Author extends Content {
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', nullable: true, default: null })
+  bio?: string;
+
+  @Column({ type: 'date', nullable: true, default: null })
+  birthDate?: Date;
+
+  @ManyToMany(() => Book, (book) => book.authors)
+  books: Book[];
+}
