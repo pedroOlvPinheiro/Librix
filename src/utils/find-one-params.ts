6ec8e998bc3aef364156890b9cd8183d1d5aa658
Pedro Oliveira <pedroolvcontato@gmail.com>
2026-04-01 13:37:08 -0300

    feat(utils): adiciona apiProperty em findOneParams para habilitar testes direto na url das requisições no swagger

--- src/utils/find-one-params.ts
+++ src/utils/find-one-params.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID } from 'class-validator';
 
 export class FindOneParams {
+  @ApiProperty({
+    description: 'ID único do recurso (UUID)',
+    example: 'd290f1ee-6c54-4b01-90e6-d701748f0851',
+  })
   @IsUUID()
   id: string;
 }
